7e210fc66ad875f1bfc8b674cfc523a60547f1bd
NThanatos <asuramirai@gmail.com>
2015-10-31 16:53:11 +0800

    minor update

--- Peekonme/sellershop.php
+++ Peekonme/sellershop.php
@@ -52,73 +52,85 @@
 
         ?>
         <div class="container" style="margin: 4em auto;">
-            <table>
-                <?php
-					$sql = "SELECT * FROM supplier_own_game WHERE Supplier_UserID = '" . $shopid   .  "'";
-					
-					if ($result = mysqli_query($connection, $sql)) 
-					{
-                    	echo $shopid;
-                        while($row = mysqli_fetch_assoc($result))
-                		{
-                            echo $shopid;
+
+            <div class="panel panel-transparent col-md-4">
+                <br><br><br>
+                <table>
+                    <thead>
+                        <tr>
+                            <th><strong>Game Name</strong></th>
+                            <th><strong>Price</strong></th>
+                            <th><strong>Stock Left</strong></th>
+                        </tr>
+                    </thead>
+
+                    <?php
+                    $sql = "SELECT * FROM supplier_own_game WHERE Supplier_UserID = '" . $shopid . "'";
+
+                    if ($result = mysqli_query($connection, $sql)) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $sql2 = "SELECT * FROM game WHERE GameID = " . $row['GameID'];
+                            if ($result2 = mysqli_query($connection, $sql2)) {
+                                $row4 = mysqli_fetch_assoc($result2);
+                            }
+                            echo '<tr>';
+                            echo '<td style="width: 100px"> ';
+                            echo $row4['Title'];
+                            echo '</td>';
+                            echo '<td style="width: 100px"> ';
+                            echo $row4['Price'];
+                            echo '</td>';
+                            echo '<td style="width: 100px"> ';
                             echo $row['Stock'];
-                           
-               	 		}
-                	}
-
-                ?>
-            </table>
-        </div>
-
-        <div class="panel panel-transparent col-md-4">
-            <br><br><br>
-            
-            <table>
-                <thead>
-                    <tr>
-                        <th><strong>Game Name</strong></th>
-                        <th><strong>Price</strong></th>
-                        <th><strong>Stock Left</strong></th>
-                    </tr>
-                </thead>
-                
-                <?php
-                $sql = "SELECT * FROM supplier_own_game WHERE Supplier_UserID = '" . $shopid . "'";
-
-                if ($result = mysqli_query($connection, $sql)) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $sql2 = "SELECT * FROM game WHERE GameID = " . $row['GameID'];
-                        if ($result2 = mysqli_query($connection, $sql2)) {
-                            $row4 = mysqli_fetch_assoc($result2);
+                            echo '</td>';
+                            echo '<td style="width: 100px"> ';
+                            echo '<input type="text" class="form-control" placeholder="amount">';
+                            echo '</td>';
+                            echo '</tr>';
                         }
-                        echo '<tr>';
-                        echo '<td style="width: 100px"> ';
-                        echo $row4['Title'];
-                        echo '</td>';
-                        echo '<td style="width: 100px"> ';
-                        echo $row4['Price'];
-                        echo '</td>';
-                        echo '<td style="width: 100px"> ';
-                        echo $row['Stock'];
-                        echo '</td>';
-                        echo '<td style="width: 100px"> ';
-                        echo '<input type="text" class="form-control" placeholder="amount">';
-                        echo '</td>';
-                        echo '</tr>';
                     }
-                }
-                ?>
-                <td><button class="btn btn-info">Pay Now</button></td>
-            </table>
-        </div>
+                    ?>
+                    <td><button class="btn btn-info" data-toggle="modal" data-target="#viewcart" >View Cart</button></td>
+                </table>
+            </div>
 
-        <div class="panel panel-transparent col-md-6">
-            <h1> <strong><?php echo $shopid; ?> </strong></h1>
-           
-        </div>
+            <div class="panel panel-transparent col-md-6">
+                <h1> <strong><?php echo $shopid; ?> </strong></h1>
+
+            </div>
 	</div>
   
     <?php include 'footer.inc.php'; ?>
+        
+        <div class="modal fade" id="viewcart" tabindex="-1" role="dialog" aria-labelledby="viewcart" style="width: auto; color: black;">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <table class="modal-title" id="myModalLabel">
+                                <tr>
+                                    <td style="width: 125px">Game Name</td>
+                                    <td style="width: 100px">Price/Unit</td>
+                                    <td style="width: 100px">Amount</td>
+                                    <td style="width: 100px">Total Cost</td>
+                                </tr>
+                        </table>
+                    </div>
+                    <div class="modal-body">
+                        <table>
+                            <p>content here</p>
+                            
+                            <tr>
+                                <td><button class="btn btn-info">Pay Now</button></td>
+                            </tr>
+                        </table>
+                        <a class="btn-group-sm"></a>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
