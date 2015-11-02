b8df3319971d9cb6dbdc9450d1efaccdd14a94ba
NThanatos <asuramirai@gmail.com>
2015-11-02 15:25:17 +0800

    sellershop page -left edit database * remember to check quantity against stock to make it key constraint *

--- Peekonme/sellershop.php
+++ Peekonme/sellershop.php
@@ -23,14 +23,18 @@
     <body>
         
         <?php
-
+        $chosenQuantity= "";
+        
         if (isset($_GET['id'])) 
-		{
+        {
             $shopid = $_GET['id'];
-        } else 
-		{
-            header('Location: imagedetail.php');
+        } 
+        
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            
+ //           $chosenQuantity = $_POST['row1'];
         }
+        
         
 //        $sql = "SELECT * FROM supplier_own_game WHERE Supplier_UserID = " . $shopid;
 //        if ($result = mysqli_query($connection, $sql)) {
@@ -64,11 +68,11 @@
 
             <div class="panel panel-transparent col-md-4">
                 <br><br><br>
-
+                <form class="form-horizontal" role="form" method="post" action="sellershop.php?id=<?php echo $shopid ?>">
                 <table>
                     <thead>
                         <tr>
-                            <th><strong>Game Name</strong></th>
+                            <th><strong>Game Name <?php echo $shopid; ?></strong></th>
                             <th><strong>Price</strong></th>
                             <th><strong>Stock Left</strong></th>
                         </tr>
@@ -95,59 +99,80 @@
                             echo '<td style="width: 100px"> ';
                             echo $row['Stock'];
                             echo '</td>';
-                            echo '<td style="width: 100px"> ';
-                            echo '<input type="text" class="form-control" placeholder="amount" method="get" name="row ' .$rowCounter. '">';
-                            //$chosenQuantity =  $_GET["row$rowCounter"];
-                            //echo $rowCounter;
-                            //echo $chosenQuantity;
+                            echo '<td style="width: 150px"> ';
+                            echo '<input type="text" class="form-control" placeholder="amount" id="row' .$rowCounter. '" name="row' .$rowCounter. '">';
                             echo '</td>';
                             echo '</tr>';
                         }
                     }
                     ?>
-                    <td><button class="btn btn-info" data-toggle="modal" data-target="#viewcart" type="submit" >View Cart</button></td>
+                    <td><input type="submit" value="Update Cart" class="btn btn-primary" data-toggle="modal" data-target="#viewcart"/></td>
                 </table>
-
+            </form>
             </div>
 
             <div class="panel panel-transparent col-md-6">
                 <h1> <strong><?php echo $shopid; ?> </strong></h1>
 
             </div>
-	</div>
-  
-    <?php include 'footer.inc.php'; ?>
-        
-        <div class="modal fade" id="viewcart" tabindex="-1" role="dialog" aria-labelledby="viewcart" style="width: auto; color: black;">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <table class="modal-title" id="myModalLabel">
-                                <tr>
+            <div style="float:right" class="panel panel-transparent col-md-4">
+                <h1> <strong>Shopping Cart</strong></h1>
+               <div class="table-hover">
+<!--            the below div change to form and run sql to edit database-->
+                   <div>
+                       <table class="modal-title" id="myModalLabel">
+                                <tr class="modal-header">
                                     <td style="width: 125px">Game Name</td>
                                     <td style="width: 100px">Price/Unit</td>
                                     <td style="width: 100px">Amount</td>
                                     <td style="width: 100px">Total Cost</td>
                                 </tr>
+                            <?php 
+                            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                if ($result = mysqli_query($connection, $sql)) {
+                                    $x = 0;
+                                    $totalcost = 0;
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        $sql2 = "SELECT * FROM game WHERE GameID = " . $row['GameID'];
+                                        if ($result2 = mysqli_query($connection, $sql2)) {
+                                            $row4 = mysqli_fetch_assoc($result2);
+                                        }
+                                        $x++;
+                                        $chosenQuantity = $_POST['row'.$x.''];
+                                        if ($chosenQuantity > 0 ){
+                                        echo '<tr>';
+                                        echo '<td style="width: 100px"> ';
+                                        echo $row4['Title'];
+                                        echo '</td>';
+                                        echo '<td style="width: 100px"> ';
+                                        echo $row4['Price'];
+                                        echo '</td>';
+                                        echo '<td>';
+                                        echo $chosenQuantity;
+                                        echo '</td>';
+                                        echo '</tr>';
+                                        $totalcost =  $row4['Price'] * $chosenQuantity + $totalcost;
+                                        }
+                                    }
+                                    echo '<tr><td></td><td></td><td></td><td>';
+                                    if ($totalcost > 0){
+                                        echo $totalcost ;                                
+                                    }
+                                    echo '</td></tr>';
+                                }
+                            }
+                            ?>   
                         </table>
-                    </div>
-                    <div class="modal-body">
-                        <table>
-                            <p>content here</p>
-                            <?php $chosenQuantity ?>
-                            
-                            <tr>
-                                <td><button class="btn btn-info">Pay Now</button></td>
-                            </tr>
-                        </table>
-                        <a class="btn-group-sm"></a>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-info" data-dismiss="modal">Pay now</button>
                     </div>
-                </div>
+                </div> 
             </div>
-        </div>
+	</div>
+  
+    <?php include 'footer.inc.php'; ?>
+        
+        
     </body>
 </html>
