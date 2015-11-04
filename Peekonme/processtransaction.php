7dc5ab5e1b66cc462007571b40565a4e3df591ea
Biri Biri - YY <darkdanito@hotmail.com>
2015-11-05 00:45:54 +0800

    Version: 5.0.7
    - Refactored the code

--- Peekonme/processtransaction.php
+++ Peekonme/processtransaction.php
@@ -1,47 +1,40 @@
 <?php
-session_start();
-include 'header.inc.php';
-$totalcost = $_SESSION['totalcost'];
-$chosenQuantity = $_SESSION['chosenquantity'];
-$gametitle = $_SESSION['gametitle'];
-$supplier_userid = $_SESSION['supplier_userid'];
-$gameid = $_SESSION['gameid'];
+	session_start();
+	
+	include 'header.inc.php';
+	
+	$totalcost = $_SESSION['totalcost'];
+	$chosenQuantity = $_SESSION['chosenquantity'];
+	$gametitle = $_SESSION['gametitle'];
+	$supplier_userid = $_SESSION['supplier_userid'];
+	$gameid = $_SESSION['gameid'];
 
-//echo $totalcost;
-//echo '<br>';
-//echo $chosenQuantity;
-//echo '<br>';
-//echo $gametitle;
-//echo '<br>';
-//echo $supplier_userid;
-//echo '<br>';
-//echo $gameid;
-$sql = "UPDATE SUPPLIER_OWN_GAME SET STOCK = STOCK - ".$chosenQuantity." WHERE SUPPLIER_USERID = '".$supplier_userid."' AND GAMEID = " .$gameid ;
-if ($result = mysqli_query($connection, $sql)) {
-    $row = mysql_num_rows($result);
+	$sql = "UPDATE SUPPLIER_OWN_GAME SET STOCK = STOCK - ".$chosenQuantity." WHERE SUPPLIER_USERID = '".$supplier_userid."' AND GAMEID = " .$gameid ;
+	
+	if ($result = mysqli_query($connection, $sql)) 
+	{
+		$row = mysql_num_rows($result);
     
-    if ($row = 1) {
-         $sql = "UPDATE SUPPLIER SET TOTAL_SALES = TOTAL_SALES + ".$totalcost." WHERE USERID = '".$supplier_userid."'";
-         if($result2 = mysqli_query($connection,$sql)) {
-             $row2 = mysql_num_rows($result2);
-             if ($row2 = 1) {
-                 $_SESSION["purchasesuccess"] = "Purchase Successful !";
-            
-             }
-         }
-         
-    }
-    else {
-        $_SESSION["purchasefailed"] = "Purchase failed ! Please try again !";
-    }
+    	if ($row = 1) 
+		{
+			$sql = "UPDATE SUPPLIER SET TOTAL_SALES = TOTAL_SALES + ".$totalcost." WHERE USERID = '".$supplier_userid."'";
+			
+			if($result2 = mysqli_query($connection,$sql)) 
+			{
+            	$row2 = mysql_num_rows($result2);
+             	
+				if ($row2 = 1) 
+				{
+                	$_SESSION["purchasesuccess"] = "Purchase Successful !";
+            	}
+         	}
+      	}
+		else 
+		{
+			$_SESSION["purchasefailed"] = "Purchase failed ! Please try again !";
+		}
     header('Location: explore.php');
-    
-    
-    
-    
-}
+	}
 
-include 'footer.inc.php';
+	include 'footer.inc.php';
 ?>
-
-  
--- Peekonme/search.php
+++ Peekonme/search.php
@@ -4,9 +4,11 @@
     $con=mysql_connect("localhost","root","");
     $db=mysql_select_db("gamernationdb",$con);
     $query=mysql_query("select * from game where Title LIKE '%{$term}%'");
+	
     while($row=mysql_fetch_assoc($query))
     {
       $array[] = $row['Title'];
     }
-    echo json_encode($array);
+    
+	echo json_encode($array);
 ?>
