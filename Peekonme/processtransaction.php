1799370cf55aab4ab153adfe38d6e603629a9ebf
Winnie Lew <lewmh.winnie@yahoo.com.sg>
2015-11-06 19:25:42 +0800

    bug fixes for transaction

--- Peekonme/processtransaction.php
+++ Peekonme/processtransaction.php
@@ -1,40 +1,61 @@
 <?php
-	session_start();
-	
-	include 'header.inc.php';
-	
-	$totalcost = $_SESSION['totalcost'];
-	$chosenQuantity = $_SESSION['chosenquantity'];
-	$gametitle = $_SESSION['gametitle'];
-	$supplier_userid = $_SESSION['supplier_userid'];
-	$gameid = $_SESSION['gameid'];
+session_start();
+include 'header.inc.php';
 
-	$sql = "UPDATE SUPPLIER_OWN_GAME SET STOCK = STOCK - ".$chosenQuantity." WHERE SUPPLIER_USERID = '".$supplier_userid."' AND GAMEID = " .$gameid ;
-	
-	if ($result = mysqli_query($connection, $sql)) 
-	{
-		$row = mysql_num_rows($result);
+$arraysize =  $_SESSION['arraysize'];
+$supplier_userid = $_SESSION['supplier_userid'];
+$totalCost = $_SESSION['totalcost'];
+
+for ($i=0; $i < $arraysize; $i++) {
+    $chosenQuantity[$i] = $_SESSION['chosenquantity'][$i];
+    $gameID[$i] = $_SESSION['gameid'][$i];
+    $gameTitle[$i] = $_SESSION['gametitle'][$i];
+
+
+//echo '<br>';
+//echo 'Chosen Quantity: ' ;
+//echo $chosenQuantity[$i];
+//echo '<br>';
+//echo 'Chosen gametitle: ' ;
+//echo $gameTitle[$i];
+////echo '<br>';
+////echo 'Supplier ID: ' ;
+////cho $supplier_userid;
+//echo '<br>';
+//echo 'Game ID: ';
+//echo $gameID[$i];
+//echo '<br>';
+
+
+//echo 'Total cost: '; 
+//echo $totalCost;
+$sql = "UPDATE SUPPLIER_OWN_GAME SET STOCK = STOCK - ".$chosenQuantity[$i]." WHERE SUPPLIER_USERID = '".$supplier_userid."' AND GAMEID = " .$gameID[$i];
+
+
+if ($result = mysqli_query($connection, $sql)) {
+    $row = mysql_num_rows($result);
+}
+}
+
+    if ($row = 1) {
+    ?>
+<?php
+ 
+         $sql = "UPDATE SUPPLIER SET TOTAL_SALES = TOTAL_SALES + ".$totalCost." WHERE USERID = '".$supplier_userid."'";
+         if($result2 = mysqli_query($connection,$sql)) {
+             $row2 = mysql_num_rows($result2);
+             if ($row2 = 1) {
+                 $_SESSION["purchasesuccess"] = "Purchase Successful !";
+                            }  
+                                                        }   
+    }
+    else {
+        $_SESSION["purchasefailed"] = "Purchase failed ! Please try again !";
+          }
+        header('Location: explore.php');
     
-    	if ($row = 1) 
-		{
-			$sql = "UPDATE SUPPLIER SET TOTAL_SALES = TOTAL_SALES + ".$totalcost." WHERE USERID = '".$supplier_userid."'";
-			
-			if($result2 = mysqli_query($connection,$sql)) 
-			{
-            	$row2 = mysql_num_rows($result2);
-             	
-				if ($row2 = 1) 
-				{
-                	$_SESSION["purchasesuccess"] = "Purchase Successful !";
-            	}
-         	}
-      	}
-		else 
-		{
-			$_SESSION["purchasefailed"] = "Purchase failed ! Please try again !";
-		}
-    header('Location: explore.php');
-	}
 
-	include 'footer.inc.php';
+include 'footer.inc.php';
 ?>
+
+  
--- Peekonme/sellershop.php
+++ Peekonme/sellershop.php
@@ -13,7 +13,6 @@
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="css/main.css"/>
         <link href="css/star-rating.min.css" media="all" rel="stylesheet" type="text/css" />
-        
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script src="js/star-rating.min.js" type="text/javascript"></script>        
     </head>
@@ -31,22 +30,49 @@
             $shopid = $_GET['id'];
         } 
         
-        if ($_SERVER["REQUEST_METHOD"] == "POST") 
-		{
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
             
+ //           $chosenQuantity = $_POST['row1'];
         }
-        ?>
         
-		<div class="container" style="margin: 4em auto;">
-			<div class="panel panel-transparent col-md-4">
+        
+//        $sql = "SELECT * FROM supplier_own_game WHERE Supplier_UserID = " . $shopid;
+//        if ($result = mysqli_query($connection, $sql)) {
+//            while ($row = mysqli_fetch_assoc($result)) {
+//                $gameID = $row['GameID'];
+//                $gameStock = $row['Stock'];
+//            }
+//        }
+// need implement count         
+
+//        $sql2 = "SELECT * FROM game WHERE GameID = " . $gameID;
+//        if ($result2 = mysqli_query($connection, $sql2)) {
+//            while ($row2 = mysqli_fetch_assoc($result2)) {
+//                $gameName = $row['Title'];
+//                $gamePrice = $row['Price'];
+//
+//            }
+//        }
+//$chosenQuantity =0;
+//        if ($_SERVER["REQUEST_METHOD"] == "GET") 
+//			{
+////                $chosenName = $_GET["Name"];
+////                $chosenPrice = $_GET["Price"]; 
+////                $chosenQuantity = $_GET["row2"];
+//            for ($x = 0; $x < $rowCounter; $x++){
+//                $chosenQuantity = $_GET["row' .$x. '"];
+//            }
+//                        }
+        ?>
+        <div class="container" style="margin: 4em auto;">
+
+            <div class="panel panel-transparent col-md-4">
                 <br><br><br>
-                
                 <form class="form-horizontal" role="form" method="post" action="sellershop.php?id=<?php echo $shopid ?>">
-                
                 <table>
-					<thead>
+                    <thead>
                         <tr>
-							<th><strong>Game Name <?php echo $shopid; ?></strong></th>
+                            <th><strong>Game Name <?php echo $shopid; ?></strong></th>
                             <th><strong>Price</strong></th>
                             <th><strong>Stock Left</strong></th>
                         </tr>
@@ -55,14 +81,11 @@
                     <?php
                     $sql = "SELECT * FROM supplier_own_game WHERE Supplier_UserID = '" . $shopid . "'";
                     $_SESSION['supplier_userid'] = $shopid;
-                    if ($result = mysqli_query($connection, $sql)) 
-					{
+                    if ($result = mysqli_query($connection, $sql)) {
                         $rowCounter = 0;
-                        while ($row = mysqli_fetch_assoc($result)) 
-						{
+                        while ($row = mysqli_fetch_assoc($result)) {
                             $sql2 = "SELECT * FROM game WHERE GameID = " . $row['GameID'];
-                            if ($result2 = mysqli_query($connection, $sql2)) 
-							{
+                            if ($result2 = mysqli_query($connection, $sql2)) {
                                 $row4 = mysqli_fetch_assoc($result2);
                             }
                             $rowCounter++;
@@ -85,87 +108,112 @@
                     ?>
                     <td><input type="submit" value="Update Cart" class="btn btn-primary" data-toggle="modal" data-target="#viewcart"/></td>
                 </table>
-            	</form>
+            </form>
             </div>
 
             <div class="panel panel-transparent col-md-6">
-                <h1><strong>Seller Name: <?php echo $shopid; ?></strong></h1>
+                <h1> <strong><?php echo $shopid; ?> </strong></h1>
+
             </div>
-            
             <div style="float:right" class="panel panel-transparent col-md-4">
-				<h1><strong>Shopping Cart</strong></h1>
-               	
-                <div class="table-hover">
-				<!--	the below div change to form and run sql to edit database	-->
-					<div>
-						<table class="modal-title" id="myModalLabel">
-							<tr class="modal-header">
-                                <td style="width: 125px">Game Name</td>
-                                <td style="width: 100px">Price/Unit</td>
-                                <td style="width: 100px">Amount</td>
-                                <td style="width: 100px">Total Cost</td>
-                            </tr>
-                            
+                <h1> <strong>Shopping Cart</strong></h1>
+               <div class="table-hover">
+<!--            the below div change to form and run sql to edit database-->
+                   <div>
+                       <table class="modal-title" id="myModalLabel">
+                                <tr class="modal-header">
+                                    <td style="width: 125px">Game Name</td>
+                                    <td style="width: 100px">Price/Unit</td>
+                                    <td style="width: 100px">Amount</td>
+                                    <td style="width: 100px">Total Cost</td>
+                                </tr>
                             <?php 
-                            if ($_SERVER["REQUEST_METHOD"] == "POST") 
-							{
-                                if ($result = mysqli_query($connection, $sql)) 
-								{
+                            
+                            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                
+                                if ($result = mysqli_query($connection, $sql)) {
                                     $x = 0;
+                                    $i = 0;
                                     $totalcost = 0;
-                                    while ($row = mysqli_fetch_assoc($result)) 
-									{
+                                    while ($row = mysqli_fetch_assoc($result)) {
                                         $sql2 = "SELECT * FROM game WHERE GameID = " . $row['GameID'];
-                                        if ($result2 = mysqli_query($connection, $sql2)) 
-										{
+                                        if ($result2 = mysqli_query($connection, $sql2)) {
                                             $row4 = mysqli_fetch_assoc($result2);
                                         }
+                                        
                                         $x++;
                                         $chosenQuantity = $_POST['row'.$x.''];
-                                        if ($chosenQuantity > 0 )
-										{
-											echo '<form action="processtransaction.php" method="post">'; 
-											echo '<tr>';
-											echo '<td style="width: 100px"> ';
-											echo $row4['Title'];
-											echo '</td>';
-											echo '<td style="width: 100px"> ';
-											echo $row4['Price'];
-											echo '</td>';
-											echo '<td>';
-											echo $chosenQuantity;
-											echo '</td>';
-											echo '</tr>';
-											$totalcost =  $row4['Price'] * $chosenQuantity + $totalcost;
-											$_SESSION['gametitle'] = $row4['Title'];
-											$_SESSION['totalcost'] = $totalcost;
-											$_SESSION['chosenquantity'] = $chosenQuantity;
-											$_SESSION['gameid'] = $row4['GameID'];
+                                        if ($chosenQuantity > 0 ){
+                                        echo '<form action="processtransaction.php" method="post">'; 
+                                        echo '<tr>';
+                                        echo '<td style="width: 100px"> ';
+                                        echo $row4['Title'];
+                                       // echo $row4['Supplier_userid'];
+                                        echo '</td>';
+                                        echo '<td style="width: 100px"> ';
+                                        echo $row4['Price'];
+                                        echo '</td>';
+                                        echo '<td>';
+                                        echo $chosenQuantity;
+                                        echo '</td>';
+                                        echo '</tr>';
+                                        $totalcost =  $row4['Price'] * $chosenQuantity + $totalcost;
+//                                        $_SESSION['gametitle'] = $row4['Title'];
+//                                        $_SESSION['totalcost'] = $totalcost;
+//                                        $_SESSION['chosenquantity'] = $chosenQuantity;
+//                                        $_SESSION['gameid'] = $row4['GameID'];
+                                        
+                                        
+                                        $gameTitlearr[] = $row4['Title'];
+//                                        $totalCostarr[] = $totalcost;
+                                        $chosenQuantityarr[] = $chosenQuantity;
+                                        $gameIdarr[] = $row4['GameID'];
+                                        
+                                        $_SESSION['gametitle'] = $gameTitlearr;
+                                        $_SESSION['totalcost'] = $totalcost;
+                                        $_SESSION['chosenquantity'] = $chosenQuantityarr;
+                                        $_SESSION['gameid'] = $gameIdarr;
+                                        
+//                                        $_SESSION['gameData'][$i]['gametitle'] = $row4['Title'];
+//                                        $_SESSION['gameData'][$i]['totalcost'] = $totalcost;
+//                                        $_SESSION['gameData'][$i]['chosenquantity'] = $chosenQuantity;
+//                                        $_SESSION['gameData'][$i]['gameid'] = $row4['GameID'];
+                                        $i++;
+                                       // $_SESSION['gameData'][$i]['supplier_userid'] = $supplier_userid;
+                                        
                                         }
+                                        
                                     }
+                                    
+//                                    print_r(sizeof($gameTitlearr));
+                                    $_SESSION['arraysize'] = sizeof($gameTitlearr);
+                                    
                                     echo '<tr><td></td><td></td><td></td><td>';
-                                    if ($totalcost > 0)
-									{
+                                    if ($totalcost > 0){
                                         echo $totalcost ;                                
                                     }
                                     echo '</td></tr>';
                                 }
                             }
                             ?>   
-						</table>
-					</div>
-                    
+                        </table>
+                   </div>
                     <div class="modal-footer">
-						<input type="submit" value="Pay now" class="btn btn-primary"/>
-                        <?php 
-							echo '</form>' 
-						?>        
+                        
+                        <input type="submit" value="Pay now" class="btn btn-primary"/>
+                        <?php echo '</form>' ?>        
+                    
+                
+
                     </div>
-				</div>
-			</div>
-		</div> 
-
-    	<?php include 'footer.inc.php'; ?>
-
+                    </div>
+            </div>
+                </div> 
+            
+	
+  
+    <?php include 'footer.inc.php'; ?>
+        
+        
     </body>
 </html>
